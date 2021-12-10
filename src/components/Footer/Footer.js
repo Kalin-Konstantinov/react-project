bd2804195f067629b2c4cace551089fdf8ceeb74
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-12-10 15:06:32 +0200

    add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import style from './Footer.module.css';
+
+const Footer = () => {
+    return (
+        <footer className={style.footer}>
+            <p>
+                &copy; Created by Kalin Konstantiov.
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
