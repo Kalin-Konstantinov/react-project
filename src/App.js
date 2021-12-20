68827b3616831f5e7c0cbcc904fc63be6a41cc40
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-12-20 13:56:09 +0200

    fi add functionality to home page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import Register from './components/Register/Register';
 import Footer from './components/Footer/Footer';
+import './App.css';
 
 function App() {
   return (
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,56 @@
-import style from './Home.module.css';
+import styles from './Home.module.css';
 
 const Home = () => {
-    return <h1>Home page</h1>
+    return (
+        <section className={styles.body}>
+            <article className={styles.imgWrapper}>
+                <img src="/images/home-resize.png" className={styles.img} alt="food" />
+            </article>
+
+            <article className={styles.gallery}>
+                <h2 className={styles.title}>Recipes Gallery</h2>
+                <article className={styles.galleryContainer}>
+                    <section className={styles.cards}>
+                        <article className={styles.card}>
+                            <article className={styles.cardImg}>
+                                <img src="/images/soup.jpeg" alt="soup" className={styles.img}/>
+                            </article>
+                            <h4 className={styles.cardTitle}>Soup</h4>
+                        </article>
+                        <article className={styles.card}>
+                            <article className={styles.cardImg}>
+                                <img src="/images/sallad.jpeg" alt="soup" className={styles.img}/>
+                            </article>
+                            <h4 className={styles.cardTitle}>Sallad</h4>
+                        </article>
+                        <article className={styles.card}>
+                            <article className={styles.cardImg}>
+                                <img src="/images/dessert.jpeg" alt="soup" className={styles.img}/>
+                            </article>
+                            <h4 className={styles.cardTitle}>Dessert</h4>
+                        </article>
+                        <article className={styles.card}>
+                            <article className={styles.cardImg}>
+                                <img src="/images/smoothie.jpeg" alt="soup" className={styles.img}/>
+                            </article>
+                            <h4 className={styles.cardTitle}>Smoothie</h4>
+                        </article>
+                    </section>
+                </article>
+            </article>
+
+            <article className={styles.about}>
+                <article className={styles.goals}>
+                    <p>We alway strive to develop and share our experience. Share your experience with us! Create your own recipe and make the day happy for more people.</p>
+                </article>
+                <article className={styles.aboutImg}>
+                    <img src="/images/kitchen.jpeg" alt="kitchen" className={styles.img}/>
+                </article>
+
+            </article>
+
+        </section>
+    );
 }
 
 export default Home;
