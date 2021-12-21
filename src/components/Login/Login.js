5ffd882935fffca9d6dc18aae1f7488ffd07a097
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-12-21 18:25:43 +0200

    style login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,20 @@
+import { Link } from 'react-router-dom';
 import style from './Login.module.css';
 
 
 const Login = () => {
     return (
         <section className={style.loginWrapper}>
-            <form className={style.loginForm}>
-                <label className={style.label} htmlFor="email">
-                    Email:
-                    <input type="email" id="email" className={style.inputField} />
-                </label>
-                <label className={style.label} htmlFor="password">
-                    Password:
-                    <input type="password" id="password" className={style.inputField} />
-                </label>
-                <input type="submit" className={style.submit} value="Login" />
-            </form>
+            <section className={style.login}>
+                <h2 className={style.title}>Login</h2>
+                <form className={style.loginForm}>
+                    <input type="email" id="email" className={style.inputField} placeholder="Email" />
+                    <input type="password" id="password" className={style.inputField} placeholder="Password" />
+                    <input type="submit" className={style.submit} value="Sign up" />
+                </form>
+                <h3 className={style.question}>You don't have account?</h3>
+                <p className={style.register}>You can register <Link className={style.link} to='/register'>Here</Link></p>
+            </section>
         </section>
     );
 }
