6c7a3ec51c024a6df39ff4f1ade1d847e90477ae
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-12-21 21:31:35 +0200

    add styles to register page

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,28 +1,22 @@
+import { Link } from 'react-router-dom';
 import style from './Register.module.css';
+
 
 const Register = () => {
     return (
-        <section className={style.registerWrapper} >
-            <form className={style.registerForm}>
-                <label className={style.label} htmlFor="email">
-                    Email:
-                    <input type="email" id="email" className={style.inputField} />
-                </label>
-                <label className={style.label} htmlFor="name">
-                    Name:
-                    <input type="text" id="name" className={style.inputField} />
-                </label>
-                <label className={style.label} htmlFor="password">
-                    Password:
-                    <input type="password" id="password" className={style.inputField} />
-                </label>
-
-                <label className={style.label} htmlFor="rePassword">
-                    Repeat Password:
-                    <input type="password" id="rePassword" className={style.inputField} />
-                </label>
-                <input type="submit" className={style.submit} value="Register" />
-            </form>
+        <section className={style.registerWrapper}>
+            <section className={style.register}>
+                <h2 className={style.title}>Register</h2>
+                <form className={style.registerForm}>
+                    <input type="text" name="name" id="name" className={style.inputField} placeholder="Name" />
+                    <input type="email" id="email" name="email" className={style.inputField} placeholder="Email" />
+                    <input type="password" id="password" name="password" className={style.inputField} placeholder="Password" />
+                    <input type="password" id="rePass" name="rePass" className={style.inputField} placeholder="Confirm password" />
+                    <input type="submit" className={style.submit} value="Register" />
+                </form>
+                <h3 className={style.question}>You already have account?</h3>
+                <p className={style.login}>You can login <Link className={style.link} to='/login'>Here</Link></p>
+            </section>
         </section>
     );
 }
