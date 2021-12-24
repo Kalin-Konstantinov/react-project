002ebe47d83d93f31eabb5afde09945a510233c1
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-12-24 18:15:34 +0200

    fix class names in gallery

--- src/components/Galerry/Gallery.js
+++ src/components/Galerry/Gallery.js
@@ -7,49 +7,49 @@ const Gallery = () => {
             <h1>Choose Category</h1>
             <section class="gallert-categories">
                 <article class="gallery-categories-card">
-                    <article class="card-img">
+                    <article class="gallery-categories-card-img">
                         <img src="../images/img/Appetizer.png" alt="Appetizer" />
                     </article>
                     <h3 class="card-title">Appetizer</h3>
                 </article>
                 <article class="gallery-categories-card">
-                    <article class="card-img">
+                    <article class="gallery-categories-card-img">
                         <img src="../images/img/Dessert.png" alt="Appetizer" />
                     </article>
                     <h3 class="card-title">Dessert</h3>
                 </article>
                 <article class="gallery-categories-card">
-                    <article class="card-img">
+                    <article class="gallery-categories-card-img">
                         <img src="../images/img/Main-dish.png" alt="Appetizer" />
                     </article>
                     <h3 class="card-title">Main dish</h3>
                 </article>
                 <article class="gallery-categories-card">
-                    <article class="card-img">
+                    <article class="gallery-categories-card-img">
                         <img src="../images/img/Pasta.png" alt="Appetizer" />
                     </article>
                     <h3 class="card-title">Pasta</h3>
                 </article>
                 <article class="gallery-categories-card">
-                    <article class="card-img">
+                    <article class="gallery-categories-card-img">
                         <img src="../images/img/Sallad.png" alt="Appetizer" />
                     </article>
                     <h3 class="card-title">Sallad</h3>
                 </article>
                 <article class="gallery-categories-card">
-                    <article class="card-img">
+                    <article class="gallery-categories-card-img">
                         <img src="../images/img/Seafood.png" alt="Appetizer" />
                     </article>
                     <h3 class="card-title">Seafood</h3>
                 </article>
                 <article class="gallery-categories-card">
-                    <article class="card-img">
+                    <article class="gallery-categories-card-img">
                         <img src="../images/img/Smoothie.png" alt="Appetizer" />
                     </article>
                     <h3 class="card-title">Smoothie</h3>
                 </article>
                 <article class="gallery-categories-card">
-                    <article class="card-img">
+                    <article class="gallery-categories-card-img">
                         <img src="../images/img/Soup.png" alt="Appetizer" />
                     </article>
                     <h3 class="card-title">Soup</h3>
