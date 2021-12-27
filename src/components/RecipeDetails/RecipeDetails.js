b5f8b235c7ea0d5cdf35c242c584ac24faf8a110
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-12-27 17:07:03 +0200

    add styles on RecipeDetails component

--- src/components/RecipeDetails/RecipeDetails.js
+++ src/components/RecipeDetails/RecipeDetails.js
@@ -33,24 +33,26 @@ const RecipeDetails = () => {
     }
 
     return (
-        <section className="recipe-details">
-            <h2 className="recipe-details-title">{recipe.title}</h2>
-            <article className="recipe-details-img">
-                <img src={recipe.imageUrl} alt={recipe.title} />
-            </article>
-            <article className="recipe-details-products">
-                <h4>Products:</h4>
-                <p>{recipe.products}</p>
-            </article>
-            <article className="recipe-details-preparation">
-                <h4>Recipe preparation:</h4>
-                <p>{recipe.description}</p>
-            </article>
-            {user._id === recipe.ownerId?._id && <OwnerButtons recipe={recipe} />}
+        <section className="details">
+            <section className="recipe-details">
+                <h2 className="recipe-details-title">{recipe.title}</h2>
+                <article className="recipe-details-img">
+                    <img src={recipe.imageUrl} alt={recipe.title} />
+                </article>
+                <article className="recipe-details-products">
+                    <h4>Products:</h4>
+                    <p>{recipe.products}</p>
+                </article>
+                <article className="recipe-details-preparation">
+                    <h4>Recipe preparation:</h4>
+                    <p>{recipe.description}</p>
+                </article>
+                {user._id === recipe.ownerId?._id && <OwnerButtons recipe={recipe} />}
 
-            <article className="recipe-details-author">
-                <Link to='/Author'>Ceated by: {recipe.ownerId?.name}</Link>
-            </article>
+                <article className="recipe-details-author">
+                    <Link to='/Author'>Ceated by: {recipe.ownerId?.name}</Link>
+                </article>
+            </section>
         </section>
     );
 }
