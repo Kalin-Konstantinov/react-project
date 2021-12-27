24f13c8dbecc3bb933867ad4590743be3ca15b72
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-12-27 10:20:18 +0200

    refactor styles on CreateRecipe component

--- src/components/CreateRecipe/CreateRecipe.js
+++ src/components/CreateRecipe/CreateRecipe.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import { useAuthContext } from '../../contexts/AuthContext';
 import { createRecepe } from '../../services/recipesService';
 import {getAllCategorys} from '../../services/catalogService'
-import style from './CreateRecipe.module.css';
+import  './CreateRecipe.css';
 
 const CreateRecipe = () => {
     const [categories, setCategories] = useState([]);
@@ -46,18 +46,18 @@ const CreateRecipe = () => {
     }
 
     return (
-        <section className={style.registerWrapper}>
-            <section className={style.register}>
-                <h2 className={style.title}>Create recipe</h2>
-                <form className={style.registerForm} onSubmit={onRegisterHandler}>
-                    <input type="text" name="title" id="name" className={style.inputField} placeholder="Title" />
-                    <input type="text" name="imageUrl" className={style.inputField} placeholder="Image..." />
-                    <textarea name="products" className={style.textarea} placeholder="Add products..." />
-                    <textarea name="description" className={style.textarea} placeholder="Preparation of the recipe: description..." />
-                    <select name="category" className={style.dropdown}  >
+        <section className="create-page">
+            <section className="create-form-wrapper">
+                <h2 className="create-title">Create recipe</h2>
+                <form className="create-form" onSubmit={onRegisterHandler}>
+                    <input type="text" name="title" id="name" className="create-form-imput" placeholder="Title" />
+                    <input type="text" name="imageUrl" className="create-form-imput" placeholder="Image..." />
+                    <textarea name="products" className="create-form-textarea" placeholder="Add products..." />
+                    <textarea name="description" className="create-form-textarea" placeholder="Preparation of the recipe: description..." />
+                    <select name="category" className="create-form-dropdown"  >
                         {categories.map(x => <option value={x.name} key={x._id}>{x.name}</option>)}
                     </select>
-                    <input type="submit" className={style.submit} value="Share recipe" />
+                    <input type="submit" className="create-form-submit" value="Share recipe" />
                 </form>
             </section>
         </section>
